9834a42b7c4e73733af71fe1df89bd35695200c3
SnazzyNivesh522 <niveshpritmani@gmail.com>
2025-01-01 17:08:53 +0530

    fixed error

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig(() => {
-  return {
-    "base": "/Cloud-Storage-Frontend-React",
-    plugins: [react()],
-    server: {
-      proxy: {
-        "/api": {
-          target: import.meta.env.VITE_BACKEND_URL || "http://localhost:8000",
-          changeOrigin: true,
-        },
+export default defineConfig({
+  base: "/Cloud-Storage-Frontend-React",
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: import.meta.env.VITE_BACKEND_URL || "http://localhost:8000",
+        changeOrigin: true,
       },
     },
-  };
+  },
 });
