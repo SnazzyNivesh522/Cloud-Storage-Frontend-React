6e38c55a4b7a7c1530cb2d32a81546a8a280df2c
SnazzyNivesh522 <niveshpritmani@gmail.com>
2025-01-01 17:00:44 +0530

    updated by removing unneccesary node:process

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,14 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import process from "node:process";
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
+export default defineConfig(() => {
   return {
     plugins: [react()],
     server: {
       proxy: {
         "/api": {
-          target: env.VITE_BACKEND_URL || "http://localhost:8000",
+          target: import.meta.env.VITE_BACKEND_URL || "http://localhost:8000",
           changeOrigin: true,
         },
       },
